20f222f598d631e58473d5fdf77193a311187e7e
gutemberg234 <gute.tec@gmail.com>
2021-06-26 03:50:17 -0300

    rota id sala

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,5 +1,5 @@
 import { FormEvent, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import illustrationImg from '../assets/illustration.svg';
 import logoImg from '../assets/logo.svg';
@@ -14,7 +14,7 @@ import '../styles/auth.scss';
 
 export function NewRoom() {
     const { user } = useAuth()
-
+    const history = useHistory()
     const [newRoom, setNewRoom] = useState('');
 
     async function handleCreateRoom(event: FormEvent) {
@@ -30,6 +30,8 @@ export function NewRoom() {
             title: newRoom,
             authorId: user?.id,
         })
+
+        history.push(`/rooms/${firebaseRoom.key}`)
     }
 
     return (
