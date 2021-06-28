f9e491c20f8442e7afa55555ac2e2835082e483f
gutemberg234 <gute.tec@gmail.com>
2021-06-27 23:45:24 -0300

    Regra do banco

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,28 @@
+import logoImg from '../assets/logo.svg';
+import { Button } from '../components/Button';
+
 export function Room(){
     return (
-        <h1>Sala</h1>
-    )
+      <div id="page-room">
+          <header>
+              <div className="content">
+                  <img src={logoImg} alt="Letmeask" />
+                  <div>codigo</div>
+              </div>
+          </header>
+          <main className="content">
+              <div className="room-title">
+                  <h1>Sala React</h1>
+                  <span>4 perguntas</span>
+              </div>
+              <form>
+                  <textarea placeholder="O que você quer perguntar?" />
+                  <div className="form-footer">
+                      <span>Para eniar uma pergunta, <button>faça seu login</button>.</span>
+                      <Button type="submit">Enviar pergunta</Button>
+                  </div>
+              </form>
+          </main>
+      </div> 
+    );
 }
