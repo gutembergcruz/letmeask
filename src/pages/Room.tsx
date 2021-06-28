333d0a6da1833f2be693c0e07a2cef78703b18e3
gutemberg234 <gute.tec@gmail.com>
2021-06-28 01:29:18 -0300

    Estilização Rooms

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,6 +1,8 @@
 import logoImg from '../assets/logo.svg';
 import { Button } from '../components/Button';
 
+import '../styles/room.scss';
+
 export function Room(){
     return (
       <div id="page-room">
