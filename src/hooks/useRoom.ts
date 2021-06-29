32211654e1e42077348b5f1e832ba7056c1d028f
gutemberg234 <gute.tec@gmail.com>
2021-06-29 04:19:06 -0300

    likes

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { database } from "../services/firebase"
+import { useAuth } from "./useAuth";
 
 type FirebaseQuestions = Record<string, {
     author: {
@@ -9,6 +10,9 @@ type FirebaseQuestions = Record<string, {
     content: string;
     isAnswer: boolean;
     isHighlighted: boolean;
+    likes: Record<string, {
+        authorId: string;
+    }>
 }>
 
 type QuestionType = {
@@ -20,9 +24,12 @@ type QuestionType = {
     content: string;
     isAnswer: boolean;
     isHighlighted: boolean;
+    likeCount: number;
+    likeId: string | undefined;
 }
 
 export function useRoom(roomId: string) {
+    const { user } = useAuth();
     const [questions, setQuestions] = useState<QuestionType[]>([])
     const [title, setTitle] = useState('')
 
@@ -39,13 +46,20 @@ export function useRoom(roomId: string) {
                     content: value.content,
                     author: value.author,
                     isHighlighted: value.isHighlighted,
-                    isAnswer: value.isAnswer
+                    isAnswer: value.isAnswer,
+                    likeCount: Object.values(value.likes ?? {}).length,
+                    likeId : Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId == user?.id)?.[0],
                 }
             })
             setTitle(databaseRoom.title);
             setQuestions(parsedQuestions);
         })
-    }, [roomId]);
+
+        return () => {
+            roomRef.off('value');
+        }
+
+    }, [roomId, user?.id]);
 
     return { questions, title }
 }
---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,85 @@
+// import { useState } from 'react';
+import { useParams } from 'react-router';
+
+import logoImg from '../assets/logo.svg';
+import { Button } from '../components/Button';
+import { Question } from '../components/Question';
+import { RoomCode } from '../components/RoomCode';
+// import { useAuth } from '../hooks/useAuth';
+import { useRoom } from '../hooks/useRoom';
+// import { database } from '../services/firebase';
+
+import '../styles/room.scss';
+
+
+
+type RoomParams = {
+    id: string;
+}
+
+
+
+export function AdminRoom() {
+    // const { user } = useAuth();
+    const params = useParams<RoomParams>();
+    // const [newQuestion, setNewQuestion] = useState('')
+    const roomId = params.id;
+    
+    const { title, questions } = useRoom(roomId)
+
+    
+    // async function handleSendQuestion(event: FormEvent) {
+    //     event.preventDefault()
+
+    //     if (newQuestion.trim() === '') {
+    //         return
+    //     }
+    //     if (!user) {
+    //         throw new Error('Rou must be logged in')
+    //     }
+    //     const question = {
+    //         content: newQuestion,
+    //         author: {
+    //             name: user.name,
+    //             avatar: user.avatar,
+    //         },
+    //         isHighlighted: false,
+    //         isAnswer: false
+    //     };
+
+    //     await database.ref(`rooms/${roomId}/questions`).push(question)
+
+    //     setNewQuestion('');
+    // }
+
+    return (
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <div>
+                    <RoomCode code={params.id} />
+                    <Button isOutlined>Encerrar Sala</Button>
+                    </div>
+                </div>
+            </header>
+            <main className="content">
+                <div className="room-title">
+                    <h1>Sala {title}</h1>
+                    {questions.length > 0 && <span>{questions.length} pergunta{questions.length > 1 && 's'}</span>}
+                </div>
+                <div className="question-list">
+                    {questions.map(question => {
+                        return (
+                            <Question
+                            key={question.id}
+                                content={question.content}
+                                author={question.author}
+                            />
+                        )
+                    })}
+                </div>
+            </main>
+        </div>
+    );
+}
